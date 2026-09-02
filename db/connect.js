f52dd306d5fa0a8c4c8fd91d2f41f6b412b22130
bpace522 <pacebc522@gmail.com>
2026-09-02 10:07:05 -0600

    pointer change

--- db/connect.js
+++ db/connect.js
@@ -11,7 +11,7 @@ const initDb = (callback) => {
     }
     MongoClient.connect(process.env.MONGODB_URI)
     .then((client) => {
-        _db = client.db();
+        _db = client.db('CSE341');
         callback(null, _db);
     })
     .catch((err) => {
