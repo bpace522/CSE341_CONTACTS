a4639f3f5f94ece606917a8b40bf4a0d50a7fca6
bpace522 <pacebc522@gmail.com>
2026-09-02 09:41:28 -0600

    changes 2

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,20 +2,32 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-        const result = await mongodb.getDb().db().collection('contacts').find();
+    try {
+        const result = await mongodb.getDb().collection('contacts').find();
         result.toArray().then((contacts) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(contacts);
         }); 
+    } catch (err) {
+        res.status(500).json({ message: err.message});
+    }
 };
 
 const getOneById = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId});
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
-    });
+    try {
+
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({ message: 'Invalid Contact ID' });
+        }
+        const userId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().collection('contacts').find({ _id: userId});
+        result.toArray().then((contacts) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(contacts[0]);
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message})
+    }
 };
 
 
